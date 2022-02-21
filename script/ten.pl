cf7c2209d0fb5479d57c49fa7050ed70d4b02e35
Shenwei <92427364+Shenwei625@users.noreply.github.com>
2022-02-21 10:38:51 +0800

    Delete ten.pl

--- script/ten.pl
+++
@@ -1,8 +0,0 @@
-#! usr/bin/perl
-open LIST, '<dist2.txt' or die "Cannot write to ls.out:$!";
-open STDOUT, '>format.txt' or die "Cannot write to ls.out:$!";
-my @list = <LIST>;
-foreach (@list) {
-    s/\.[0-9]/ /m;
-    print;
-}
